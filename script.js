80c4d0e4b0f091e494679219b691e4a73ec47c56
ReDDarKwh <turbowill4@gmail.com>
2019-04-29 13:26:36 -0400

    added questions basic structure

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ const questions = [
   {
     title: 'Accueil',
     name: 'Ça commence ici!',
-    desc: 'Trouver les mots de passes pour passer au niveau suivant'
+    desc: 'Trouver les mots de passes pour passer au niveau suivant',
+    css: 'font-weight: bold;'
   },
   {
-    title: 'Niveau 1',
-    name: 'Slow start',
-    desc: 'description n1'
+    title: 'Niveau 1 ',
+    name: 'Lecture de fichier (facile)',
+    desc: 'Le mot de passe se retrouve dans le ficher /usr/home/pass'
   },
   {
-    title: 'Niveau 2',
-    name: ' slow start',
+    title: 'Niveau 2 ',
+    name: 'slow start (facile)',
     desc: 'description n2'
   },
   {
-    title: 'Niveau 3',
-    name: 'slow start',
+    title: 'Niveau 3 ',
+    name: 'slow start (facile)',
+    desc: 'description n3'
+  },
+  {
+    title: 'Niveau 4 ',
+    name: 'slow start (moyen)',
+    desc: 'description n3'
+  },
+  {
+    title: 'Niveau 5 ',
+    name: 'slow start (moyen)',
+    desc: 'description n3'
+  },
+  {
+    title: 'Niveau 6 ',
+    name: 'slow start (moyen)',
+    desc: 'description n3'
+  },
+  {
+    title: 'Niveau 7',
+    name: 'slow start (difficile)',
+    desc: 'description n3'
+  },
+  {
+    title: 'Niveau 8',
+    name: 'slow start (difficile) ',
     desc: 'description n3'
   }
 ];
@@ -35,6 +61,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   for (let q of questions) {
     let item = document.createElement('li');
     let a = document.createElement('a');
+
+    a.style = q.css;
 
     item.append(a);
     list.append(item);
