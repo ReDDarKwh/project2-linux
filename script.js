b5a8191704b990447c57f2b18261cb94bfb92573
Alexandre Arsenault <tial1212@gmail.com>
2019-05-16 12:09:27 -0400

    test3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const questions = [
   {
     title: 'Accueil',
     name: 'Bienvenue sur notre site !',
-    desc: "Le jeu consiste à trouver les mots de passe des usagers en exécutant des commandes bash pour passer au niveau suivant.<br>Pour commencer, connectez-vous en SSH a la machine <b>prj2.lan</b> avec l’usager <b>user1</b> et le mot de passe <b>93aoFiMz08sC</b> .",
+    desc: "Le jeu consiste à trouver les mots de passe des usagers en exécutant des commandes bash pour passer au niveau suivant22. <br>Pour commencer, connectez-vous en SSH a la machine <b>prj2.lan</b> avec l’usager <b>user1</b> et le mot de passe <b>93aoFiMz08sC</b> .",
     css: 'font-weight: bold;'
   },
   {
